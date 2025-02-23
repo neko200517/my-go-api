8d538056d1aa12697e261223646f50c92d0fd626
Jun Terayama <pam130901@gmail.com>
2025-02-23 20:33:51 +0900

    fix config.yml

--- src/main.go
+++ src/main.go
@@ -1,41 +1,42 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-		"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    r := gin.Default()
+	r := gin.Default()
 
-		// Cors
-		r.Use(cors.New(cors.Config {
-			// アクセスを許可したいアクセス元
-			AllowOrigins: []string {
-				"http://localhost:5173",
-			},
-			// アクセスを許可したいHTTPメソッド
-			AllowMethods: []string {
-				"POST",
-				"GET",
-				"OPTIONS",
-			},
-			// 許可したいHTTPリクエストヘッダ
-			AllowHeaders: []string {
-				"Access-Control-Allow-Credentials",
-				"Access-Control-Allow-Headers",
-				"Content-Type",
-				"Content-Length",
-				"Accept-Encoding",
-				"Authorization",
-			},
-		}))
+	// Cors
+	r.Use(cors.New(cors.Config{
+		// アクセスを許可したいアクセス元
+		AllowOrigins: []string{
+			"http://localhost:5173",
+			"https://d3bbw7rbcyeqvs.cloudfront.net",
+		},
+		// アクセスを許可したいHTTPメソッド
+		AllowMethods: []string{
+			"POST",
+			"GET",
+			"OPTIONS",
+		},
+		// 許可したいHTTPリクエストヘッダ
+		AllowHeaders: []string{
+			"Access-Control-Allow-Credentials",
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"Authorization",
+		},
+	}))
 
-    // GET
-    r.GET("/", func(c *gin.Context) {
-        c.JSON(200, gin.H{"message": "Hello Go Lang!!"})
-    })
+	// GET
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Hello Go Lang!!"})
+	})
 
-    // 8080ポートでサーバー起動
-    r.Run(":8080")
+	// 8080ポートでサーバー起動
+	r.Run(":8080")
 }
